d0ac31b969bec8211ed1633c3e6e9d8a3ecb727b
Bloodlister <bloodlisterer@gmail.com>
2018-12-03 15:17:10 +0000

    .

--- src/main.js
+++ src/main.js
@@ -5,10 +5,6 @@ import store from './store';
 import router from './router';
 import App from './App.vue';
 
-if (!process.env.CONNECTION_STRING) {
-  axios.defaults.baseURL = 'http://localhost:5000';
-  axios.defaults.withCredentials = false;
-}
 Vue.use(VueAxios, axios);
 
 new Vue({
